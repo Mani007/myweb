c2c252496601371cd1a6401d94da5e1ff344ba91
Mohnish <mohnish.a007@gmail.com>
2024-02-13 19:08:55 +0000

    3d model spotlight added successfully

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,7 +10,14 @@ const Computers = () => {
     
     <mesh>
     <hemisphereLight intensity={0.15} groundColor='black' />
-    
+    <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
