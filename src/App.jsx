8f582bcbe57aa25d82c7039da347d76686e3e3c7
Mohnish <mohnish.a007@gmail.com>
2024-02-13 01:51:10 +0000

    Navbar with logo

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,15 @@ function App() {
           <Navbar/>
           <Hero/>
         </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className='relative z-0'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
       </div>
     </BrowserRouter>
     </>
